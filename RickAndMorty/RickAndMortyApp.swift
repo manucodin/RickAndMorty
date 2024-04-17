a24d564ec67c668dedca32d4aaed58ad37f2dbb8
Manuel Rodríguez Sebastián <mrsebastian@idealista.com>
2024-04-17 19:04:59 +0200

    First commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Manuel Rodriguez Sebastian on 17/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
